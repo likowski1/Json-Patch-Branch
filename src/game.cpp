CleverRaven/Cataclysm-DDA

Move dimension path handling to path info proper.

src/game.cpp
@@ -13340,6 +13340,20 @@ cata_path PATH_INFO::world_base_save_path_path()
     return world_generator->active_world->folder_path_path();
 }
 
+cata_path PATH_INFO::current_dimension_save_path_path()
+{
+    std::string dimension_prefix = g->get_dimension_prefix();
+    if( !dimension_prefix.empty() ) {
+        return PATH_INFO::world_base_save_path_path() / "dimensions" / dimension_prefix;
+    }
+    return PATH_INFO::world_base_save_path_path();
+}
+
+cata_path PATH_INFO::current_dimension_player_save_path_path()
+{
+    return PATH_INFO::current_dimension_save_path_path() / base64_encode( get_avatar().get_save_id() );
+}
+
 void game::shift_destination_preview( const point &delta )
 {
     for( tripoint_bub_ms &p : destination_preview ) {
@@ -13837,6 +13851,7 @@ void game::climb_down_using( const tripoint &examp, climbing_aid_id aid_id, bool
         }
     }
 }
+
 namespace cata_event_dispatch
 {
 void avatar_moves( const tripoint &old_abs_pos, const avatar &u, const map &m )

src/map_memory.cpp
@@ -5,9 +5,7 @@
 #include "cuboid_rectangle.h"
 #include "debug.h"
 #include "filesystem.h"
-#include "game.h"
 #include "line.h"
-#include "map.h"
 #include "map_memory.h"
 #include "path_info.h"
 #include "string_formatter.h"
@@ -21,11 +19,7 @@ static constexpr int MM_SIZE = MAPSIZE * 2;
 
 static cata_path find_mm_dir()
 {
-    std::string dimension_prefix = g->get_dimension_prefix();
-    if( dimension_prefix.empty() ) {
-        return PATH_INFO::player_base_save_path_path() / "mm1";
-    }
-    return PATH_INFO::player_base_save_path_path() / "dimensionss" /  dimension_prefix / ".mm1";
+    return PATH_INFO::current_dimension_player_save_path_path() + ".mm1";
 }
 
 static cata_path find_region_path( const cata_path &dirname, const tripoint &p )

src/overmapbuffer.cpp
@@ -71,14 +71,7 @@ int camp_reference::get_distance_from_bounds() const
 
 cata_path overmapbuffer::terrain_filename( const point_abs_om &p )
 {
-    std::string dimension_prefix = g->get_dimension_prefix();
-    if( !dimension_prefix.empty() ) {
-        return PATH_INFO::world_base_save_path_path() / "dimensions" / dimension_prefix /
-               string_format( "o.%d.%d",
-                              p.x(), p.y() );
-    } else {
-        return PATH_INFO::world_base_save_path_path() / string_format( "o.%d.%d", p.x(), p.y() );
-    }
+    return PATH_INFO::current_dimension_save_path_path() / string_format( "o.%d.%d", p.x(), p.y() );
 }
 
 cata_path overmapbuffer::player_filename( const point_abs_om &p )
@@ -312,8 +305,7 @@ overmap *overmapbuffer::get_existing( const point_abs_om &p )
         // checked in a previous call of this function).
         return nullptr;
     }
-    assure_dir_exist( PATH_INFO::world_base_save_path_path() / "dimensions" /
-                      g->get_dimension_prefix() );
+    assure_dir_exist( PATH_INFO::current_dimension_save_path_path() );
     if( file_exist( terrain_filename( p ) ) ) {
         // File exists, load it normally (the get function
         // indirectly call overmap::open to do so).

src/path_info.h
@@ -96,6 +96,8 @@ cata_path user_keybindings();
 cata_path user_moddir_path();
 cata_path user_sound();
 cata_path world_base_save_path_path();
+cata_path current_dimension_save_path_path();
+cata_path current_dimension_player_save_path_path();
 
 void set_datadir( const std::string &datadir );
 void set_config_dir( const std::string &config_dir );

likowski1/Json-Patch-Branch

src/game.cpp
@@ -13838,6 +13838,20 @@ cata_path PATH_INFO::world_base_save_path()
     return world_generator->active_world->folder_path();
 }
 
+cata_path PATH_INFO::current_dimension_save_path()
+{
+    std::string dimension_prefix = g->get_dimension_prefix();
+    if( !dimension_prefix.empty() ) {
+        return PATH_INFO::world_base_save_path() / "dimensions" / dimension_prefix;
+    }
+    return PATH_INFO::world_base_save_path();
+}
+
+cata_path PATH_INFO::current_dimension_player_save_path()
+{
+    return PATH_INFO::current_dimension_save_path() / base64_encode( get_avatar().get_save_id() );
+}
+
 void game::shift_destination_preview( const point_rel_ms &delta )
 {
     for( tripoint_bub_ms &p : destination_preview ) {
@@ -14337,6 +14351,7 @@ void game::climb_down_using( const tripoint_bub_ms &examp, climbing_aid_id aid_i
         }
     }
 }
+
 namespace cata_event_dispatch
 {
 void avatar_moves( const tripoint_abs_ms &old_abs_pos, const avatar &u, const map &m )

src/map_memory.cpp
@@ -5,9 +5,7 @@
 #include "cuboid_rectangle.h"
 #include "debug.h"
 #include "filesystem.h"
-#include "game.h"
 #include "line.h"
-#include "map.h"
 #include "map_memory.h"
 #include "path_info.h"
 #include "string_formatter.h"
@@ -27,11 +25,7 @@ static tripoint_abs_sm mmr_to_sm_copy( const tripoint &p )
 
 static cata_path find_mm_dir()
 {
-    std::string dimension_prefix = g->get_dimension_prefix();
-    if( dimension_prefix.empty() ) {
-        return PATH_INFO::player_base_save_path_path() / "mm1";
-    }
-    return PATH_INFO::player_base_save_path_path() / "dimensionss" /  dimension_prefix / ".mm1";
+    return PATH_INFO::current_dimension_player_save_path() + ".mm1";
 }
 
 static cata_path find_region_path( const cata_path &dirname, const tripoint &p )

src/overmapbuffer.cpp
@@ -76,14 +76,7 @@ int camp_reference::get_distance_from_bounds() const
 
 cata_path overmapbuffer::terrain_filename( const point_abs_om &p )
 {
-    std::string dimension_prefix = g->get_dimension_prefix();
-    if( !dimension_prefix.empty() ) {
-        return PATH_INFO::world_base_save_path_path() / "dimensions" / dimension_prefix /
-               string_format( "o.%d.%d",
-                              p.x(), p.y() );
-    } else {
-        return PATH_INFO::world_base_save_path_path() / string_format( "o.%d.%d", p.x(), p.y() );
-    }
+    return PATH_INFO::current_dimension_save_path() / string_format( "o.%d.%d", p.x(), p.y() );
 }
 
 cata_path overmapbuffer::player_filename( const point_abs_om &p )
@@ -317,8 +310,7 @@ overmap *overmapbuffer::get_existing( const point_abs_om &p )
         // checked in a previous call of this function).
         return nullptr;
     }
-    assure_dir_exist( PATH_INFO::world_base_save_path_path() / "dimensions" /
-                      g->get_dimension_prefix() );
+    assure_dir_exist( PATH_INFO::current_dimension_save_path() );
     if( file_exist( terrain_filename( p ) ) ) {
         // File exists, load it normally (the get function
         // indirectly call overmap::open to do so).

src/path_info.h
@@ -92,6 +92,8 @@ cata_path user_keybindings();
 cata_path user_moddir_path();
 cata_path user_sound();
 cata_path world_base_save_path();
+cata_path current_dimension_save_path();
+cata_path current_dimension_player_save_path();
 
 void set_datadir( const std::string &datadir );
 void set_config_dir( const std::string &config_dir );